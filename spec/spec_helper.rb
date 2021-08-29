c42b065a7ed968af5afb5135fbefe25c7a1a91e4
casualnick <bartekstafiej2@gmail.com>
2021-08-29 18:40:27 +0200

    init commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+
+RSpec.configure do |config|
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.expect_with :rspec do |expectations|
+
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+
+    mocks.verify_partial_doubles = true
+  end
+
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+  
+      # Keep as many of these lines as are necessary:
+      with.library :active_record
+      with.library :active_model
+    end
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
