8a20c77a8a7cb1e212b14252150f8ebb6cbade3c
Wayne E <bartekstafiej2@gmail.com>
2021-10-30 18:16:12 +0200

    merge main to staging

--- Gemfile
+++ Gemfile
@@ -1,38 +1,39 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.0'
 
-gem 'action_policy', '~> 0.5'
-gem 'rails', '~> 6.1.4'
+gem 'blueprinter', '~> 0.25'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'dry-equalizer', '~> 0.3.0'
+gem 'dry-validation', '~> 1.6.0'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
-gem 'bootsnap', '>= 1.4.4', require: false
+gem 'rails', '~> 6.1.4'
 gem 'rswag-api'
 gem 'rswag-ui', '~> 2.4'
-gem 'dry-validation', '~> 0.6.0'
-gem 'blueprinter', '~> 0.25'
 
 group :development, :test do
-  gem 'rspec-rails', '~> 3.4', '>= 3.4.2'
   gem 'database_cleaner', '~> 1.5', '>= 1.5.3'
-  gem 'shoulda-matchers', '~> 5.0'
-  gem 'faker', '~> 1.6', '>= 1.6.6'
-  gem 'rswag-specs'
   gem 'dotenv-rails', '~> 2.1', '>= 2.1.1'
   gem 'factory_bot', '~> 6.2'
   gem 'factory_bot_rails', '~> 6.2'
+  gem 'faker', '~> 1.6', '>= 1.6.6'
   gem 'pry', '~> 0.13.0'
   gem 'pry-byebug', '~> 3.4'
+  gem 'rspec-rails', '~> 3.4', '>= 3.4.2'
+  gem 'rswag-specs'
+  gem 'shoulda-matchers', '~> 5.0'
 end
 
 group :development do
-  gem 'rubocop-rails', '~> 2.11', require: false
-  gem 'rubocop', require: false
-  gem 'rubocop-performance', require: false
   gem 'listen', '~> 3.3'
+  gem 'rubocop', '~> 1.20.0 ', require: false
+  gem 'rubocop-performance', '~> 1.11', require: false
+  gem 'rubocop-rails', '~> 2.11', require: false
   gem 'spring'
 end
 
-
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -5,7 +7,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -33,6 +35,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -60,4 +63,27 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  Shoulda::Matchers.configure do |c|
+    c.integrate do |with|
+      with.test_framework :rspec
+
+      # Keep as many of these lines as are necessary:
+      with.library :active_record
+      with.library :active_model
+    end
+  end
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
